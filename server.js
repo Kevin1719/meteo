6d747f8ddf766a0883c6037e8daf957755f6a774
Kevin1719 <kevin21.aps2b@gmail.com>
2022-08-22 12:01:56 +0300

    Third commit

--- server.js
+++ server.js
@@ -2,13 +2,13 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-let port = process.env.PORT
+
 app.use(express.static('./dist/'))
 
 app.get('/*', (req, res) => {
     res.sendFile('index.html', { root: 'dist/index.html' })
 });
 
-app.listen(port | 3000, () => {
+app.listen(process.env.PORT | 3000, () => {
     console.log('app is running on the port 3000');
 })
